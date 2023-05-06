e08b7e5f71c83651b3364955a66c5e587ac59d85
Hammadkhan34 <hammad124345@gmail.com>
2023-05-07 03:01:27 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Hammad's website! It's a pleasure to have you here.</h1>
+      <h1>Welcome to Hammad website.</h1>
     </div>
   )
 }
