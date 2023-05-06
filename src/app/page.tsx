dba2a138ca4b8a98916e7d45015c9532397b6375
Hammadkhan34 <hammad124345@gmail.com>
2023-05-07 01:38:22 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   
-          <h3>Welcome to Hammad's website! It's a pleasure to have you here.</h3>
+    <div>
+      <h1>Welcome to Hammad's website! It's a pleasure to have you here.</h1>
+    </div>
   )
 }
