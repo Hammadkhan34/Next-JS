0f052e91e0f81a66bc03ba37665263f5459ddedc
Hammadkhan34 <hammad124345@gmail.com>
2023-05-07 03:44:37 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
    
-          <h3>Hello hammad bhai</h3>
+          <h3>Greetings from Hammad, a proud student of Batch 42 studying the fascinating worlds of Web 3.0 and the Metaverse!</h3>
   )
 }
